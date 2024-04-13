65ad24f927105917dd3dafb6c070d13fe5791f7a
SME\avagy <avagytan.samvel02@gmail.com>
2024-04-13 22:05:39 +0400

    Update MongoDB to MySQL/Sequelize
    - Replace Mongoose with Sequelize for database connection
    - Update app.js to connect to MySQL and sync models
    - Install mysql2 and sequelize packages

--- src/models/Form.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const formDataSchema = new mongoose.Schema({
-	data: mongoose.Schema.Types.Mixed, // 'Mixed' type for a flexible object
-});
-
-const Form = mongoose.model("Form", formDataSchema);
-
-module.exports = Form;
